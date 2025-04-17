5e6bc8bf03fb4861cb9594fd5a2d343c81cffe71
Vensan Cabardo <vensancabardo@gmail.com>
2025-04-16 20:30:13 -0700

    package.json updates

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  devIndicators: false,
+  assetPrefix: isProd ? '/vensancabardo.github.io/' : '',
+  basePath: isProd ? '/vensancabardo.github.io' : '',
+  output: 'export'
 };
 
 export default nextConfig;
